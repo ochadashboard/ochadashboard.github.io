265d1767a539510a417ec3ef965f041a1474cd77
Bernard Nandwa <nandwabee@gmail.com>
2014-05-06 04:03:44 +0300

    kenya funding

--- assets/components/library/jquery/ochacustom.js
+++ assets/components/library/jquery/ochacustom.js
@@ -36,4 +36,26 @@ $(document).ready(function () {
             }]
         });
     });
+    $(function() {
+    var chart = new Highcharts.Chart({
+
+        chart: {
+            renderTo: 'kenyatrends-container',
+            type: 'column'
+        },
+        yAxis: {
+            title: {
+                text: 'axis title',
+                useHTML: true,
+                style: {
+                    "-webkit-transform": "rotate(90deg)",
+                    "-moz-transform": "rotate(90deg)", 
+                    "-o-transform": "rotate(90deg)"
+                }
+            }
+        },
+        series: [{
+            data: [23,45,12,89,123,12,5]}]
+
+    });
 });
