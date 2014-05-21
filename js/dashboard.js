c406df8d8007225bde72dc813da9ba0c9381d71e
Bernard Nandwa <nandwabee@gmail.com>
2014-05-21 18:34:33 +0300

    commit all changes

--- js/dashboard.js
+++ js/dashboard.js
@@ -180,7 +180,64 @@
         }
 
 
-
+        $('#donorcontainer').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Top Five Donors'
+            },
+            subtitle: {
+                text: 'Source: fts.unocha.org'
+            },
+            xAxis: {
+                categories: ['United States', 'United Kingdom', 'Norway', 'Denmark', 'Sweden'],
+                title: {
+                    text: null
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Population (millions)',
+                    align: 'high'
+                },
+                labels: {
+                    overflow: 'justify'
+                }
+            },
+            tooltip: {
+                valueSuffix: ' millions'
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true
+                    }
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'top',
+                x: -40,
+                y: 100,
+                floating: true,
+                borderWidth: 1,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor || '#FFFFFF'),
+                shadow: true
+            },
+            credits: {
+                enabled: false
+            },
+            colors: [
+            '#1fb5ac'
+            ],
+            series: [{
+                showInLegend:false,
+                data: [1107, 310, 305, 203, 200]
+            },]
+        });
 
         if (Morris.EventEmitter) {
             // Use Morris.Area instead of Morris.Line
