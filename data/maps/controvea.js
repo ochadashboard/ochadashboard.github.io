419c6c4d97730f4ca2826cb44fed6bc012e26e03
Bernard Nandwa <nandwabee@gmail.com>
2014-06-26 14:49:53 +0300

    fix this

---
+++ data/maps/controvea.js
@@ -0,0 +1,484 @@
+Highcharts.maps["custom/world"] = {
+    "title": "World",
+    "version": "1.0.1",
+    "type": "FeatureCollection",
+    "copyright": "Copyright (c) 2014 Highsoft AS, Based on data from Natural Earth http://www.naturalearthdata.com",
+    "copyrightShort": "Natural Earth",
+    "copyrightUrl": "http://www.naturalearthdata.com",
+    "features": [ {
+        "type": "Feature",
+        "id": "DJ",
+        "properties": {
+            "hc-group": "admin0",
+            "hc-middle-x": 0.56,
+            "hc-middle-y": 0.55,
+            "hc-key": "dj",
+            "hc-a2": "DJ",
+            "name": "Djibouti",
+            "labelrank": "5",
+            "country-abbrev": "Dji.",
+            "subregion": "Eastern Africa",
+            "woe-id": "23424797",
+            "region-wb": "Middle East & North Africa",
+            "iso-a3": "DJI",
+            "iso-a2": "DJ",
+            "region-un": "Africa",
+            "continent": "Africa"
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [1044, 1360],
+                    [1039, 1360],
+                    [1038, 1360],
+                    [1039, 1361],
+                    [1038, 1361],
+                    [1038, 1363],
+                    [1041, 1367],
+                    [1043, 1366],
+                    [1045, 1368],
+                    [1046, 1365],
+                    [1045, 1362],
+                    [1044, 1360]
+                ]
+            ]
+        }
+    }, {
+        "type": "Feature",
+        "id": "UG",
+        "properties": {
+            "hc-group": "admin0",
+            "hc-middle-x": 0.44,
+            "hc-middle-y": 0.49,
+            "hc-key": "ug",
+            "hc-a2": "UG",
+            "name": "Uganda",
+            "labelrank": "3",
+            "country-abbrev": "Uga.",
+            "subregion": "Eastern Africa",
+            "woe-id": "23424974",
+            "region-wb": "Sub-Saharan Africa",
+            "iso-a3": "UGA",
+            "iso-a2": "UG",
+            "region-un": "Africa",
+            "continent": "Africa"
+        },
+        "geometry": {
+            "type": "MultiPolygon",
+            "coordinates": [
+                [
+                    [
+                        [1001, 1309],
+                        [993, 1308],
+                        [991, 1303],
+                        [986, 1303],
+                        [984, 1302],
+                        [982, 1301],
+                        [980, 1301],
+                        [980, 1303],
+                        [980, 1305],
+                        [982, 1307],
+                        [981, 1307],
+                        [982, 1311],
+                        [984, 1313],
+                        [989, 1316],
+                        [988, 1318],
+                        [986, 1319],
+                        [986, 1324],
+                        [999, 1325],
+                        [1001, 1328],
+                        [1006, 1315],
+                        [1001, 1309]
+                    ]
+                ],
+                [
+                    [
+                        [991, 1303],
+                        [991, 1303],
+                        [991, 1303],
+                        [991, 1303]
+                    ]
+                ]
+            ]
+        }
+    },  {
+        "type": "Feature",
+        "id": "ET",
+        "properties": {
+            "hc-group": "admin0",
+            "hc-middle-x": 0.77,
+            "hc-middle-y": 0.55,
+            "hc-key": "et",
+            "hc-a2": "ET",
+            "name": "Ethiopia",
+            "labelrank": "2",
+            "country-abbrev": "Eth.",
+            "subregion": "Eastern Africa",
+            "woe-id": "23424808",
+            "region-wb": "Sub-Saharan Africa",
+            "iso-a3": "ETH",
+            "iso-a2": "ET",
+            "region-un": "Africa",
+            "continent": "Africa"
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [1038, 1361],
+                    [1038, 1361],
+                    [1038, 1360],
+                    [1039, 1360],
+                    [1044, 1360],
+                    [1049, 1350],
+                    [1068, 1346],
+                    [1053, 1331],
+                    [1039, 1326],
+                    [1034, 1328],
+                    [1028, 1324],
+                    [1010, 1329],
+                    [997, 1347],
+                    [1002, 1353],
+                    [1006, 1364],
+                    [1013, 1376],
+                    [1020, 1379],
+                    [1034, 1375],
+                    [1041, 1367],
+                    [1038, 1363],
+                    [1038, 1361]
+                ]
+            ]
+        }
+    }, {
+        "type": "Feature",
+        "id": "RW",
+        "properties": {
+            "hc-group": "admin0",
+            "hc-middle-x": 0.48,
+            "hc-middle-y": 0.55,
+            "hc-key": "rw",
+            "hc-a2": "RW",
+            "name": "Rwanda",
+            "labelrank": "3",
+            "country-abbrev": "Rwa.",
+            "subregion": "Eastern Africa",
+            "woe-id": "23424937",
+            "region-wb": "Sub-Saharan Africa",
+            "iso-a3": "RWA",
+            "iso-a2": "RW",
+            "region-un": "Africa",
+            "continent": "Africa"
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [984, 1302],
+                    [986, 1300],
+                    [985, 1296],
+                    [981, 1294],
+                    [977, 1295],
+                    [977, 1295],
+                    [977, 1296],
+                    [979, 1298],
+                    [978, 1300],
+                    [979, 1300],
+                    [980, 1301],
+                    [982, 1301],
+                    [984, 1302]
+                ]
+            ]
+        }
+    }, {
+        "type": "Feature",
+        "id": "KE",
+        "properties": {
+            "hc-group": "admin0",
+            "hc-middle-x": 0.04,
+            "hc-middle-y": 0.48,
+            "hc-key": "ke",
+            "hc-a2": "KE",
+            "name": "Kenya",
+            "labelrank": "2",
+            "country-abbrev": "Ken.",
+            "subregion": "Eastern Africa",
+            "woe-id": "23424863",
+            "region-wb": "Sub-Saharan Africa",
+            "iso-a3": "KEN",
+            "iso-a2": "KE",
+            "region-un": "Africa",
+            "continent": "Africa"
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [1037, 1300],
+                    [1031, 1293],
+                    [1026, 1285],
+                    [1019, 1293],
+                    [1002, 1303],
+                    [1003, 1305],
+                    [1001, 1309],
+                    [1006, 1315],
+                    [1001, 1328],
+                    [1008, 1331],
+                    [1010, 1329],
+                    [1028, 1324],
+                    [1034, 1328],
+                    [1039, 1326],
+                    [1035, 1321],
+                    [1035, 1303],
+                    [1037, 1300]
+                ]
+            ]
+        }
+    },{
+        "type": "Feature",
+        "id": "BI",
+        "properties": {
+            "hc-group": "admin0",
+            "hc-middle-x": 0.59,
+            "hc-middle-y": 0.47,
+            "hc-key": "bi",
+            "hc-a2": "BI",
+            "name": "Burundi",
+            "labelrank": "6",
+            "country-abbrev": "Bur.",
+            "subregion": "Eastern Africa",
+            "woe-id": "23424774",
+            "region-wb": "Sub-Saharan Africa",
+            "iso-a3": "BDI",
+            "iso-a2": "BI",
+            "region-un": "Africa",
+            "continent": "Africa"
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [981, 1286],
+                    [979, 1290],
+                    [978, 1292],
+                    [979, 1293],
+                    [977, 1295],
+                    [981, 1294],
+                    [985, 1296],
+                    [986, 1293],
+                    [981, 1286]
+                ]
+            ]
+        }
+    },{
+        "type": "Feature",
+        "id": "SO",
+        "properties": {
+            "hc-group": "admin0",
+            "hc-middle-x": 0.73,
+            "hc-middle-y": 0.29,
+            "hc-key": "so",
+            "hc-a2": "SO",
+            "name": "Somalia",
+            "labelrank": "6",
+            "country-abbrev": "Som.",
+            "subregion": "Eastern Africa",
+            "woe-id": "-90",
+            "region-wb": "Sub-Saharan Africa",
+            "iso-a3": "SOM",
+            "iso-a2": "SO",
+            "region-un": "Africa",
+            "continent": "Africa"
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [1073, 1361],
+                    [1084, 1364],
+                    [1082, 1353],
+                    [1068, 1329],
+                    [1059, 1319],
+                    [1052, 1315],
+                    [1037, 1300],
+                    [1035, 1303],
+                    [1035, 1321],
+                    [1039, 1326],
+                    [1053, 1331],
+                    [1068, 1346],
+                    [1073, 1352],
+                    [1073, 1361]
+                ]
+            ]
+        }
+    }, {
+        "type": "Feature",
+        "id": "SO",
+        "properties": {
+            "hc-group": "admin0",
+            "hc-middle-x": 0.50,
+            "hc-middle-y": 0.50,
+            "hc-key": "so",
+            "hc-a2": "SO",
+            "name": "Somaliland",
+            "labelrank": "5",
+            "country-abbrev": "Solnd.",
+            "subregion": "Eastern Africa",
+            "woe-id": "-99",
+            "region-wb": "Sub-Saharan Africa",
+            "iso-a3": "-99",
+            "iso-a2": "SO",
+            "region-un": "Africa",
+            "continent": "Africa"
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [1045, 1362],
+                    [1050, 1357],
+                    [1073, 1361],
+                    [1073, 1352],
+                    [1068, 1346],
+                    [1049, 1350],
+                    [1044, 1360],
+                    [1045, 1362]
+                ]
+            ]
+        }
+    },{
+        "type": "Feature",
+        "id": "ER",
+        "properties": {
+            "hc-group": "admin0",
+            "hc-middle-x": 0.07,
+            "hc-middle-y": 0.22,
+            "hc-key": "er",
+            "hc-a2": "ER",
+            "name": "Eritrea",
+            "labelrank": "4",
+            "country-abbrev": "Erit.",
+            "subregion": "Eastern Africa",
+            "woe-id": "23424806",
+            "region-wb": "Sub-Saharan Africa",
+            "iso-a3": "ERI",
+            "iso-a2": "ER",
+            "region-un": "Africa",
+            "continent": "Africa"
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [1013, 1376],
+                    [1016, 1389],
+                    [1023, 1394],
+                    [1029, 1380],
+                    [1035, 1377],
+                    [1045, 1368],
+                    [1043, 1366],
+                    [1041, 1367],
+                    [1034, 1375],
+                    [1020, 1379],
+                    [1013, 1376]
+                ]
+            ]
+        }
+    }, {
+        "type": "Feature",
+        "id": "SS",
+        "properties": {
+            "hc-group": "admin0",
+            "hc-middle-x": 0.77,
+            "hc-middle-y": 0.48,
+            "hc-key": "ss",
+            "hc-a2": "SS",
+            "name": "South Sudan",
+            "labelrank": "3",
+            "country-abbrev": "S. Sud.",
+            "subregion": "Eastern Africa",
+            "woe-id": "-99",
+            "region-wb": "Sub-Saharan Africa",
+            "iso-a3": "SSD",
+            "iso-a2": "SS",
+            "region-un": "Africa",
+            "continent": "Africa"
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [1001, 1328],
+                    [999, 1325],
+                    [986, 1324],
+                    [980, 1330],
+                    [970, 1332],
+                    [965, 1339],
+                    [954, 1349],
+                    [959, 1357],
+                    [966, 1353],
+                    [977, 1352],
+                    [982, 1356],
+                    [988, 1354],
+                    [997, 1366],
+                    [1002, 1353],
+                    [997, 1347],
+                    [1010, 1329],
+                    [1008, 1331],
+                    [1001, 1328]
+                ]
+            ]
+        }
+    },{
+        "type": "Feature",
+        "id": "SD",
+        "properties": {
+            "hc-group": "admin0",
+            "hc-middle-x": 0.71,
+            "hc-middle-y": 0.50,
+            "hc-key": "sd",
+            "hc-a2": "SD",
+            "name": "Sudan",
+            "labelrank": "3",
+            "country-abbrev": "Sudan",
+            "subregion": "Northern Africa",
+            "woe-id": "-90",
+            "region-wb": "Sub-Saharan Africa",
+            "iso-a3": "SDN",
+            "iso-a2": "SD",
+            "region-un": "Africa",
+            "continent": "Africa"
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [954, 1349],
+                    [952, 1355],
+                    [948, 1360],
+                    [944, 1370],
+                    [949, 1383],
+                    [953, 1383],
+                    [953, 1401],
+                    [958, 1404],
+                    [958, 1414],
+                    [988, 1414],
+                    [1015, 1414],
+                    [1018, 1397],
+                    [1023, 1394],
+                    [1016, 1389],
+                    [1013, 1376],
+                    [1006, 1364],
+                    [1002, 1353],
+                    [997, 1366],
+                    [988, 1354],
+                    [982, 1356],
+                    [977, 1352],
+                    [966, 1353],
+                    [959, 1357],
+                    [954, 1349]
+                ]
+            ]
+        }
+    },]
+}
