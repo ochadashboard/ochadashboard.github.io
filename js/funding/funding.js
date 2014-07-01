20b2efd18a16fa5c5f8e170dce9f5fc38a40014f
Bernard Nandwa <nandwabee@gmail.com>
2014-07-01 12:27:37 +0300

    New charts for country summary

--- js/funding/funding.js
+++ js/funding/funding.js
@@ -4,5 +4,192 @@ $(document).ready(function(){
 	            $('.fundingcountry').hide();
 	            $('#' + $(this).val()).show();
 	        });
-	    });
+	 });
+
+	//Kenya Funding Chart
+	$(function () {
+		var hovertext = 'Million';
+        $('#kenyafundingsummary').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: null
+            },
+            xAxis: {
+                labels:{
+                  enabled: false
+                },
+                 lineWidth: 0,
+                minorGridLineWidth: 0,
+                lineColor: 'transparent',
+                minorTickLength: 0,
+                tickLength: 0
+             
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: null
+                },
+                labels:{
+                  enabled: false
+                },
+                lineWidth: 0,
+                minorGridLineWidth: 0,
+                lineColor: 'transparent',
+                minorTickLength: 0,
+                tickLength: 0,
+                gridLineWidth: 0
+            },
+            credits: {
+            enabled:false
+            },
+            tooltip: {
+                formatter: function() {
+                var tooltip;
+                
+                 tooltip =  '<span style="color:' + this.series.color + '">' + this.series.name + '</span>: $<b>' + this.y + '</b> Million<br/>';
+               
+                return tooltip;
+            	}
+            },
+            legend: {
+                reversed: true,
+                enabled:false
+            },
+            
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            },
+                series: [{
+                name: 'Funded',
+                data: [5,]
+            }, {
+                name: 'Unfunded',
+                data: [2,]
+            }]
+        });
+    });
+
+	//Somalia Funding Chart
+	$(function () {
+        $('#somaliafundingsummary').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: null
+            },
+            xAxis: {
+                labels:{
+                  enabled: false
+                },
+                 lineWidth: 0,
+                minorGridLineWidth: 0,
+                lineColor: 'transparent',
+                minorTickLength: 0,
+                tickLength: 0
+             
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: null
+                },
+                labels:{
+                  enabled: false
+                },
+                lineWidth: 0,
+                minorGridLineWidth: 0,
+                lineColor: 'transparent',
+                minorTickLength: 0,
+                tickLength: 0,
+                gridLineWidth: 0
+            },
+            credits: {
+            enabled:false
+            },
+            legend: {
+                reversed: true,
+                enabled:false
+            },
+            
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            },
+                series: [{
+                name: 'Funded',
+                data: [5,]
+            }, {
+                name: 'Unfunded',
+                data: [2,]
+            }]
+        });
+    });
+
+	//Uganda Funding Chart
+	$(function () {
+        $('#ugandafundingsummary').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: null
+            },
+            xAxis: {
+                labels:{
+                  enabled: false
+                },
+                 lineWidth: 0,
+                minorGridLineWidth: 0,
+                lineColor: 'transparent',
+                minorTickLength: 0,
+                tickLength: 0
+             
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: null
+                },
+                labels:{
+                  enabled: false
+                },
+                lineWidth: 0,
+                minorGridLineWidth: 0,
+                lineColor: 'transparent',
+                minorTickLength: 0,
+                tickLength: 0,
+                gridLineWidth: 0
+            },
+            credits: {
+            enabled:false
+            },
+            legend: {
+                reversed: true,
+                enabled:false
+            },
+            
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            },
+                series: [{
+                name: 'Funded',
+                data: [5,]
+            }, {
+                name: 'Unfunded',
+                data: [2,]
+            }]
+        });
+    });
+
+
+
 });
